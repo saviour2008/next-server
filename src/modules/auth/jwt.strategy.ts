cee122890997c8fd3b8c3eb4c8fc51395ce118e3
saviour2008 <saviour2008@126.com>
2022-05-09 08:55:28 +0800

    删除无用的comment

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -4,8 +4,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { jwtConstants } from './constants';
 import { RoleType } from '../user/users.entity';
-import { Result } from 'src/common/dto/result.dto';
-import { ErrorCode } from 'src/common/exception/error.code';
 import { UsersService } from '../user/users.service';
 
 @Injectable()
@@ -23,7 +21,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     console.log(`JWT验证 - Step 4: 被守卫调用，${JSON.stringify(payload)}`);
     const user = await this.usersService.findByLogin(payload.name);
     if (user.role !== RoleType.Admin) {
-      console.log(123);
       throw new UnauthorizedException('该用户不是admin，没有权限');
     }
     return user;
